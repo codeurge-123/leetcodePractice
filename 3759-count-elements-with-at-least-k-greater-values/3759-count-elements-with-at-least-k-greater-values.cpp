a9260ed173e5c7f229ad4123d6f7f95220cbd730
code_urge <bansalyash316@gmail.com>
2026-01-01 05:38:40 +0530

    Time: 135 ms (29.29%), Space: 173.9 MB (75.9%) - LeetHub

--- 3759-count-elements-with-at-least-k-greater-values/3759-count-elements-with-at-least-k-greater-values.cpp
+++ 3759-count-elements-with-at-least-k-greater-values/3759-count-elements-with-at-least-k-greater-values.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
     int countElements(vector<int>& nums, int k) {
-        sort(nums.begin(), nums.end());
-        int n = nums.size();
-        int ans = 0;
-
-        for (int i = 0; i < n; i++) {
-            int idx = upper_bound(nums.begin(), nums.end(), nums[i]) - nums.begin();
-
-            if (n - idx >= k) {
-                ans++;
+        sort(nums.begin(),nums.end());
+        int count = 0;
+        for(int i  = 0;i<nums.size();i++) {
+            // finding the strictly greater number index
+            int index = upper_bound(nums.begin(),nums.end(),nums[i])-nums.begin();
+            if(nums.size() - index >= k) {
+                count++;
             }
         }
-        return ans;
+
+        return count;
+
     }
 };
